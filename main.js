26fa48ca598827bf74577f66460b980c2cab5928
DaliaZegaya <daliazegaya56@gmail.com>
2022-06-14 16:45:32 +0300

    commit

---
+++ main.js
@@ -0,0 +1,54 @@
+
+// //תרגיל 1
+// function someArrayFunc (myArray) {
+//     console.log(myArray.sort());
+// }
+// someArrayFunc(["hey","wassup","apple", "good"])
+
+// //תרגיל 2
+// function sortNumber (numArray) {
+//     console.log(numArray.sort((a,b)=>a-b));
+
+// }
+// sortNumber ([12,50,85,9,23,1])
+
+// //תרגיל 3
+// function sortMaxNumber (numArray) {
+//     console.log(numArray.sort((a,b)=>b-a));
+// }
+// sortMaxNumber([5,80,75,2,25,95])
+
+// //תרגיל 4
+// let userName= document.getElementById ("name_input")
+// let classNames= []
+// function classNames1() {
+//     if (classNames.length<10) {
+//         classNames.push(userName.value)
+
+//     }
+//     classNames.sort()
+//     console.log(classNames);
+//     someFunc()
+// }
+
+// //תרגיל 5
+// let agesArray= []
+// function agesFunc () {
+//     for (let i=0; i<10; i++) {
+//         if (agesArray.length<10) {
+//         agesArray.push(Math.floor(Math.random()*88))
+//         agesArray.sort()
+//     }
+//     }
+//     console.log(agesArray)
+// }
+// agesFunc()
+
+// //תרגיל 6
+// function someFunc () {
+//     let myString= classNames.join("-")
+//     console.log((myString));
+// }
+
+
+
